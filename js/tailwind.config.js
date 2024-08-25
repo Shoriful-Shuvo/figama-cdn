80d2e79e87e447df3a0133ac3fa03800634b92b1
Shoriful-Shuvo <shuvo696727xyz@gmail.com>
2024-08-25 18:38:15 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          bg: 'rgba(255, 255, 255, 0.2)',
+        },
+        maxWidth: {
+            container: '1320px',
+          },
+      }
+    }
+  }
